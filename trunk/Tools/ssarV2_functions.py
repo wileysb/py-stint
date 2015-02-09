0ee7faba7cbfec09a1c0ece1ce50a95a46cb06bb
wiley.bogren@gmail.com <wiley.bogren@gmail.com@2960c573-8b41-b8c4-a79d-8a45a2a98ebf>
2015-02-09 12:15:45 +0000

    ssarV2: dev: final intersector/writer: debugging

--- trunk/Tools/ssarV2_functions.py
+++ trunk/Tools/ssarV2_functions.py
@@ -190,6 +190,7 @@ def Isect_mod_clim_ssar(project):
 
                                         climate_rows_to_write.add((climate_id, climate_x_ind, climate_y_ind))
                                         modis_rows_to_write.add((modis_id, modis_area, modis_ctr_x, modis_ctr_y, modis_x_ind, modis_y_ind))
+                                        print modis_id, modis_area, modis_ctr_x, modis_ctr_y, modis_x_ind, modis_y_ind
 
                                         if new_ssar_csv==True:
                                             # open ssar csv, write header
@@ -422,8 +423,7 @@ def Write_modis_tile(project, modis_rows_to_write, tile_out_fmt):
             # write rows:
             for row in range(len(modis_rows)):
                 modis_id, area, ctr_x, ctr_y, x_ind, y_ind = modis_rows[row]
-                print hdf[sds].shape, ':',y_ind, x_ind
-                print modis_id, area, ctr_x, ctr_y
+                print hdf[sds].shape, ':', modis_rows[row]
                 modis_series = hdf[sds][:,y_ind,x_ind]
                 modis_row =  [modis_id, x_ind, y_ind, ctr_x, ctr_y, area]  + list(np.where(modis_series==ds_nan,np.nan,modis_series)*scale+offset)
                 mod_csv.writerow(modis_row)
